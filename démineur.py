bd0899f0c33a9b80420e8b8a8c45cb9a5eba082d
Ewen Le Bihan <hey@ewen.works>
2021-07-04 15:06:13 +0200

    🚧 Implement click-to-place and click-to-hit, accuracy, win condition

--- démineur.py
+++ démineur.py
@@ -1,5 +1,8 @@
-from tkinter import Tk, N, W, E, S
+from tkinter import Label, StringVar, Tk, N, W, E, S, Widget
+import tkinter
 from tkinter.ttk import Frame, Button
+from functools import partial
+from typing import Any, Callable
 
 # Constantes
 SUNKEN = 2
@@ -13,7 +16,7 @@ STATE_NAMES = {
 }
 
 
-class Démineur:
+class Board:
     def __init__(self, title: str, grid_size: int):
         self.grid_size = grid_size
         self.cells = []
@@ -21,39 +24,90 @@ class Démineur:
         self.root = Tk()
         self.root.title = title
 
-        self.mainframe = Frame(self.root, padding="3 3 12 12")
+        self.mainframe = Frame(self.root, width=self.grid_size*10, height=self.grid_size*10)
         self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-        self.root.columnconfigure(0)
-        self.root.rowconfigure(0)
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
+        self.hits_count = self.misses_count = 0
 
         for x in range(self.grid_size):
             self.cells.append([])
             for y in range(self.grid_size):
                 cell = Button(self.mainframe, text=f"{x}:{y}")
+                cell.bind('<Button-1>', partial(self.handle, self.switch_occupied_empty, x, y))
+                cell.bind('<Button-3>', partial(self.handle, self.hit, x, y))
                 self.cells[x].append(cell)
                 cell.grid(row=x, column=y)
+        
+        Label(self.mainframe, textvariable=StringVar(value=0))
 
 
     def __matmul__(self, new_state):
         """
-        Sets the state to the one given.
-        A state is represented as a 2D array of integers:
+        Sets the entire board's state to the one given.
+        A state is represented as a 2D array of state integers (see Démineur.change_cell)
+        """
+        for x, y in doublerange(self.grid_size):
+            self.change_cell(x, y, new_state[x][y])
+    
+    def handle(self,  method: Callable[[int, int], Any], x: int, y: int, _: tkinter.Event,) -> Any:
+        """
+        Used to bind events with tkinter's .bind()
+        Usage: 
+        ```python
+        <thing>.bind('<mapping>', partial(self.handle, self.<method to bind>, x, y))
+        ```
+        """
+        return method(x, y)
+        
+    
+    def change_cell(self, x:int, y:int, state: int):
+        """
+        Sets the state of the specified cell.
+        A state is represented as an integer:
         - 2 (or SUNKEN) represents a sunken spot
         - 1 (or OCCUPIED) represents a boat spot
         - 0 (or EMPTY) represents a water spot
         """
-        print(f"{new_state=}")
-        for x, y in doublerange(self.grid_size):
-            print(f"{x=}, {y=}")
-            self.cells[x][y].configure(text=STATE_NAMES[new_state[x][y]])
+        self.cells[x][y].configure(text=STATE_NAMES[state])
+    
+    def switch_occupied_empty(self, x:int, y:int):
+        self.change_cell(x, y, OCCUPIED if self.state_of(x, y) == EMPTY else EMPTY)
+    
+    def hit(self, x: int, y: int) -> bool:
+        """
+        Returns whether it hit a non-sunken boat or not
+        """
+        self.hits_count += 1
+        if self.state_of(x, y) == OCCUPIED:
+            self.change_cell(x, y, SUNKEN)
+            return True
+        else:
+            self.misses_count += 1
+            return False
 
     @property
     def state(self) -> list[list[int]]:
         state_repr = [[EMPTY] * self.grid_size] * self.grid_size
         for x, y in doublerange(self.grid_size):
-            state_repr[x][y] = {v:k for k,v in STATE_NAMES.items()}[self.cells[x][y]]
+            state_repr[x][y] = self.state_of(x, y)
         return state_repr
+    
+    def state_of(self, x:int, y:int) -> int:
+        return {v:k for k,v in STATE_NAMES.items()}[self.cells[x][y]['text']]
+
+    @property
+    def won(self) -> bool:
+        return not any(cell_state == OCCUPIED for cell_state in self.state)
+    
+    @property
+    def accuracy(self) -> float:
+        try:
+            return (self.hits_count - self.misses_count) / self.hits_count
+        except ZeroDivisionError:
+            return 0
 
     def __call__(self, initial_state=None):
         self @ (initial_state or [[EMPTY] * self.grid_size] * self.grid_size)
@@ -69,5 +123,5 @@ def doublerange(outer, inner=None):
 
 
 if __name__ == "__main__":
-    démineur = Démineur(title="Démineur", grid_size=10)
+    démineur = Board(title="Démineur", grid_size=10)
     démineur()
